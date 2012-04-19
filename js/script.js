9a9bc78556832be6778309359cea37a36b16f3d3
Frank Radocaj <frank@radocaj.com>
2012-04-19 16:20:52 +1000

    Added JSON parser; Fixed cross origin ajax issue

--- js/script.js
+++ js/script.js
@@ -37,18 +37,19 @@ Views
 window.RWS.Views.AppView = Backbone.View.extend({
     initialize: function (options) {
         _.extend(this, Backbone.Events);
-		_.bindAll(this, '_addIndexDefinitionView');
+        _.bindAll(this, '_addIndexDefinitionView');
         this._indexDefinitionViews = [];
-		options.app.indexDefinitions.on('add', this._addIndexDefinitionView);
-		
-		//$('#database-tabs').tab('show')
+        app.indexDefinitions.on('add', this._addIndexDefinitionView, this);
+
+        //$('#database-tabs').tab('show')
     },
 
-	_addIndexDefinitionView: function (indexDefinition) {
-		var indexDefinitionView = new RWS.Views.IndexDefinitionView({ indexDefinition: indexDefinition });
-		this._indexDefinitionViews.push(indexDefinitionView);
-		$('#index-definition-list').append(indexDefinitionView.render().el);
-	}
+    _addIndexDefinitionView: function (indexDefinition) {
+        console.log(indexDefinition);
+        var indexDefinitionView = new RWS.Views.IndexDefinitionView({ indexDefinition: indexDefinition });
+        this._indexDefinitionViews.push(indexDefinitionView);
+        $('#index-definition-list').append(indexDefinitionView.render().el);
+    }
 });
 
 window.RWS.Views.IndexDefinitionView = Backbone.View.extend({
@@ -56,13 +57,13 @@ window.RWS.Views.IndexDefinitionView = Backbone.View.extend({
 
     initialize: function (options) {
         _.extend(this, Backbone.Events);
-		this._indexDefinition = options.indexDefinition;
+        this._indexDefinition = options.indexDefinition;
     },
 
-	render: function () {
-		this.$el.append(ich.indexSummaryTemplate(this._indexDefinition.toJSON()));
-		return this;
-	}
+    render: function () {
+        this.$el.append(ich.indexSummaryTemplate(this._indexDefinition.toJSON()));
+        return this;
+    }
 });
 
 /*
@@ -92,35 +93,37 @@ window.RWS.Router = Backbone.Router.extend({
 window.RWS.App = Backbone.Model.extend({
     initialize: function () {
         _.extend(this, Backbone.Events);
-		_.bindAll(this, 'showIndexDefinitions', 'showDocuments');
-		this.indexDefinitions = new RWS.Collections.IndexDefinitions();
-		this.appView = new RWS.Views.AppView({ app: this });
-		this.router = new RWS.Router();
+        _.bindAll(this, 'showIndexDefinitions', 'showDocuments');
+        this.indexDefinitions = new RWS.Collections.IndexDefinitions();
+        window.app = this;
+        this.appView = new RWS.Views.AppView();
+        this.router = new RWS.Router();
     },
 
-	showIndexDefinitions: function () {
-		var self = this;
-		$.ajax({
-		   type: 'GET',
-		   url:'http://localhost:8080/indexes',
-		   success: function(data, textStatus, XMLHttpRequest){
-				console.log(data);
-				self.indexDefinitions.add(data);
-		        //alert(XMLHttpRequest.getResponseHeader('some_header'));
-		   },
-		   error: function (XMLHttpRequest, textStatus, errorThrown) {
-				alert(errorThrown)
-		        //alert(XMLHttpRequest.getResponseHeader('some_header'));
-		   }
-		  });
-	},
-	
-	showDocuments: function () {
-		
-	}
+    showIndexDefinitions: function () {
+        var self = this;
+        $.ajax({
+            type: 'GET',
+            dataType: 'jsonp',
+            jsonp: 'jsonp', // “jsonp”, this is needed since jQuery defaults the name of the callback parameter to “callback”. Raven expects this to be “jsonp” hence the override is needed.
+            url: 'http://localhost:8080/indexes',
+            success: function (data, textStatus, jqXHR) {
+                _.each(data, function (indexDefinition) {
+                    app.indexDefinitions.add(indexDefinition);
+                });
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(errorThrown)
+            }
+        });
+    },
+
+    showDocuments: function () {
+
+    }
 });
 
 $(function() {
-	window.app = new RWS.App();
+	new RWS.App();
 	Backbone.history.start({ pushState: false });
 });
